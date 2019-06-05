2758a3d55d6432979dcd1cd98018ea511d2418a2
James Schaffer <jas.schaffer@gmail.com>
2019-06-05 15:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var thePreFunk = funkyFunction();
-var theFunk = window.thePreFunk
+var theFunk = thePreFunk();
